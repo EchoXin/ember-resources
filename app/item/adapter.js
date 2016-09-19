1f8716f02c2a94c62a79de70f988f8aaa0bd2213
EchoXin <xin.yi@husky.neu.edu>
2016-09-19 16:36:28 -0400

    add create item
    add item adapter so that to post item on host/lists/list_id/items

---
+++ app/item/adapter.js
@@ -0,0 +1,14 @@
+import ApplicationAdapter from 'ga-wdi-boston.ember-resources/application/adapter';
+
+export default ApplicationAdapter.extend({
+  createRecord (store, type, record) {
+    let api = this.get('host');
+    let serialized = this.serialize(record, { includeId: true });
+
+    // let listId = serialized.list_id;
+    let listId = 2;
+    let url = `${api}/lists/${listId}/items`;
+    let data = { item: serialized };
+    return this.ajax(url, 'POST', { data });
+  },
+});
